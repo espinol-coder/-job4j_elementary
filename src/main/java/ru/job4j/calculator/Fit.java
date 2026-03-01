883d80ad86326d1f4440084185f92d49c156ddea
roma <theta.spain@gmail.com>
2026-03-01 22:38:30 +0100

    3. Арифметические операции. [#185 #161397] 2026

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -13,7 +13,7 @@ public class Fit {
     }
 
     public static void main(String[] args) {
-        short height = 187;
+        short height = 184;
         double man = Fit.manWeight(height);
         double woman = Fit.womanWeight(height);
         System.out.println("Man 187 is " + man);
